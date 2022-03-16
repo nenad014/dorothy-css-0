902add0e3d88323375f3870c5f3c69d974f45ab4
Nenad Nikolic <nendn.dizajn@gmail.com>
2022-03-16 23:46:19 +0100

    Added card

---
+++ sandbox.js
@@ -0,0 +1,64 @@
+// Carousel
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName('carousel-item');
+    let dots = document.getElementsByClassName('carousel-dot');
+    if(n > slides.length) {
+        slideIndex = 1;
+    }
+    if(n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for(i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    for(i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(' active', '');
+    }
+
+    slides[slideIndex-1].style.display = 'block';
+    dots[slideIndex-1].className += ' active';
+}
+
+// Modal
+let modal = document.getElementsByClassName('modal');
+let openModalButton = document.getElementsByClassName('open-modal');
+let closeModal = document.getElementsByClassName('close-modal');
+
+[...openModalButton].forEach((btn, ind) => {
+    btn.onclick = () => (modal[ind].style.display = 'block');
+});
+[...closeModal].forEach((close, ind) => {
+    close.onclick = () => (modal[ind].style.display = 'none');
+});
+window.onclick = (e) => {
+    [...modal].forEach((singleModal) => {
+        if(e.target === singleModal) {
+            singleModal.style.display = 'none';
+        }
+    });
+}
+
+/* openModalButton.onclick = function() {
+    modal.style.display = 'block';
+}
+closeModal.onclick = function() {
+    modal.style.display = 'none';
+}
+window.onclick = function(event) {
+    if(event.target == modal) {
+        modal.style.display = 'none';
+    }
+} */
